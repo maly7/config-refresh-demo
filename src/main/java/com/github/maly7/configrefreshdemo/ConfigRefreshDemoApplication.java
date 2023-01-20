0774bd57117122763389a513478abca58b705b4f
maly7 <paly@vmware.com>
2023-01-20 13:10:31 -0500

    add initializr project

---
+++ src/main/java/com/github/maly7/configrefreshdemo/ConfigRefreshDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.maly7.configrefreshdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigRefreshDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigRefreshDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/maly7/configrefreshdemo/ConfigRefreshDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maly7.configrefreshdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigRefreshDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
