073bfc13212c127a0f7b4dc09f086f91b49db1b7
maly7 <paly@vmware.com>
2023-01-20 15:23:11 -0500

    deployment stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,8 @@ group = "com.github.maly7"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
+extra["springCloudVersion"] = "2022.0.0"
+
 repositories {
 	mavenCentral()
 }
@@ -15,10 +17,21 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 }
 
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+springBoot {
+	buildInfo()
 }
